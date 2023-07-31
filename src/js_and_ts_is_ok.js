b39ace7708f57a6b1361d62fe7eaf45cd99dcdc0
ttsukagoshi <55706659+ttsukagoshi@users.noreply.github.com>
2023-07-31 09:32:27 +0900

    Disable no-undef for module.exports

--- src/js_and_ts_is_ok.js
+++ src/js_and_ts_is_ok.js
@@ -4,6 +4,8 @@ function hello(name) {
   console.log('Hello ' + name);
 }
 
+/* eslint-disable no-undef */
 if (typeof modules !== 'undefined') {
   modules.exports = hello;
 }
+/* eslint-enable no-undef */
